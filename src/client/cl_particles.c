0a5c01acc7b2010e8dfb69a61097d478f7c10f69
Brian Pedersen <brianfrostpedersen@gmail.com>
2016-07-09 09:52:59 +0200

    cl_particles refactored

--- src/client/cl_particles.c
+++ src/client/cl_particles.c
@@ -30,19 +30,12 @@ cparticle_t *active_particles, *free_particles;
 cparticle_t particles[MAX_PARTICLES];
 int cl_numparticles = MAX_PARTICLES;
 
-void
-CL_ClearParticles(void)
-{
-	int i;
-
+void CL_ClearParticles(void) {
 	free_particles = &particles[0];
 	active_particles = NULL;
-
-	for (i = 0; i < cl_numparticles; i++)
-	{
+	for (int i = 0; i < cl_numparticles; i++) {
 		particles[i].next = &particles[i + 1];
 	}
-
 	particles[cl_numparticles - 1].next = NULL;
 }
 
@@ -60,246 +53,89 @@ cparticle_t* CL_GetParticle(float time, float alpha, float alphavel){
     return p;
 }
 
-void
-CL_ParticleEffect(vec3_t org, vec3_t dir, int color, int count)
-{
-	int i, j;
-	cparticle_t *p;
-	float d;
+void CL_GenericParticleEffect(vec3_t org, vec3_t dir, int color, int count, int numcolors,
+                              int dirspread, float alphavel, float gravity) {
+	for (int i = 0; i < count; i++) {
+        cparticle_t* p = CL_GetParticle(cl.time, 1.0f, -1.0f / (0.5f + frandk() * alphavel));
+        if(!p){
+            return;
+        }
 
-	for (i = 0; i < count; i++)
-	{
-		if (!free_particles)
-		{
-			return;
-		}
+        p->color = numcolors > 1 ? color + (randk() & numcolors) : color;
+		float d = (float)(randk() & dirspread);
 
-		p = free_particles;
-		free_particles = p->next;
-		p->next = active_particles;
-		active_particles = p;
-
-		p->time = cl.time;
-		p->color = color + (randk() & 7);
-		d = randk() & 31;
-
-		for (j = 0; j < 3; j++)
-		{
+		for (int j = 0; j < 3; j++) {
 			p->org[j] = org[j] + ((randk() & 7) - 4) + d * dir[j];
 			p->vel[j] = crandk() * 20;
 		}
 
 		p->accel[0] = p->accel[1] = 0;
-		p->accel[2] = -PARTICLE_GRAVITY + 0.2f;
-		p->alpha = 1.0;
-
-		p->alphavel = -1.0 / (0.5 + frandk() * 0.3);
+		p->accel[2] = gravity;
 	}
 }
 
-void
-CL_ParticleEffect2(vec3_t org, vec3_t dir, int color, int count)
-{
-	int i, j;
-	cparticle_t *p;
-	float d;
-	float time;
-
-	time = (float)cl.time;
-
-	for (i = 0; i < count; i++)
-	{
-		if (!free_particles)
-		{
-			return;
-		}
-
-		p = free_particles;
-		free_particles = p->next;
-		p->next = active_particles;
-		active_particles = p;
-
-		p->time = time;
-		p->color = color + (randk() & 7);
-
-		d = randk() & 7;
-
-		for (j = 0; j < 3; j++)
-		{
-			p->org[j] = org[j] + ((randk() & 7) - 4) + d * dir[j];
-			p->vel[j] = crandk() * 20;
-		}
-
-		p->accel[0] = p->accel[1] = 0;
-		p->accel[2] = -PARTICLE_GRAVITY;
-		p->alpha = 1.0;
-
-		p->alphavel = -1.0f / (0.5f + frandk() * 0.3f);
-	}
+void CL_ParticleEffect(vec3_t org, vec3_t dir, int color, int count) {
+    CL_GenericParticleEffect(org, dir, color, count, 7, 31, 0.3f, -PARTICLE_GRAVITY + 0.2f);
 }
 
-void
-CL_ParticleEffect3(vec3_t org, vec3_t dir, int color, int count)
-{
-	int i, j;
-	cparticle_t *p;
-	float d;
-	float time;
-
-	time = (float)cl.time;
-
-	for (i = 0; i < count; i++)
-	{
-		if (!free_particles)
-		{
-			return;
-		}
-
-		p = free_particles;
-		free_particles = p->next;
-		p->next = active_particles;
-		active_particles = p;
-
-		p->time = time;
-		p->color = color;
-
-		d = randk() & 7;
-
-		for (j = 0; j < 3; j++)
-		{
-			p->org[j] = org[j] + ((randk() & 7) - 4) + d * dir[j];
-			p->vel[j] = crandk() * 20;
-		}
-
-		p->accel[0] = p->accel[1] = 0;
-		p->accel[2] = PARTICLE_GRAVITY;
-		p->alpha = 1.0;
-
-		p->alphavel = -1.0f / (0.5f + frandk() * 0.3f);
-	}
+void CL_ParticleEffect2(vec3_t org, vec3_t dir, int color, int count) {
+    CL_GenericParticleEffect(org, dir, color, count, 7, 7, 0.3f, -PARTICLE_GRAVITY);
 }
 
-void
-CL_AddParticles(void)
-{
-	cparticle_t *p, *next;
+void CL_ParticleEffect3(vec3_t org, vec3_t dir, int color, int count) {
+    CL_GenericParticleEffect(org, dir, color, count, 0, 7, 0.3f, PARTICLE_GRAVITY);
+}
+
+void CL_AddParticles(void) {
 	float alpha;
-	float time, time2;
-	vec3_t org;
-	int color;
-	cparticle_t *active, *tail;
+	float time;
 
-	active = NULL;
-	tail = NULL;
+	cparticle_t* active = NULL;
+	cparticle_t* tail = NULL;
 
-	for (p = active_particles; p; p = next)
-	{
+	cparticle_t *next;
+	for (cparticle_t* p = active_particles; p; p = next) {
 		next = p->next;
-
-		if (p->alphavel != INSTANT_PARTICLE)
-		{
+		if (p->alphavel != INSTANT_PARTICLE) {
 			time = (cl.time - p->time) * 0.001;
 			alpha = p->alpha + time * p->alphavel;
-
-			if (alpha <= 0)
-			{
+			if (alpha <= 0) {
 				/* faded out */
 				p->next = free_particles;
 				free_particles = p;
 				continue;
 			}
-		}
-		else
-		{
+		} else {
 			time = 0.0f;
 			alpha = p->alpha;
 		}
 
 		p->next = NULL;
-
-		if (!tail)
-		{
+		if (!tail) {
 			active = tail = p;
-		}
-
-		else
-		{
+		} else {
 			tail->next = p;
 			tail = p;
 		}
 
-		if (alpha > 1.0f)
-		{
+		if (alpha > 1.0f) {
 			alpha = 1;
 		}
 
-		color = p->color;
-		time2 = time * time;
+		int color = p->color;
+		float time2 = time * time;
 
+        vec3_t org;
 		org[0] = p->org[0] + p->vel[0] * time + p->accel[0] * time2;
 		org[1] = p->org[1] + p->vel[1] * time + p->accel[1] * time2;
 		org[2] = p->org[2] + p->vel[2] * time + p->accel[2] * time2;
 
 		V_AddParticle(org, color, alpha);
 
-		if (p->alphavel == INSTANT_PARTICLE)
-		{
+		if (p->alphavel == INSTANT_PARTICLE) {
 			p->alphavel = 0.0;
 			p->alpha = 0.0;
 		}
 	}
-
 	active_particles = active;
 }
-
-void
-CL_GenericParticleEffect(vec3_t org, vec3_t dir, int color,
-		int count, int numcolors, int dirspread, float alphavel)
-{
-	int i, j;
-	cparticle_t *p;
-	float d;
-	float time;
-
-	time = (float)cl.time;
-
-	for (i = 0; i < count; i++)
-	{
-		if (!free_particles)
-		{
-			return;
-		}
-
-		p = free_particles;
-		free_particles = p->next;
-		p->next = active_particles;
-		active_particles = p;
-
-		p->time = time;
-
-		if (numcolors > 1)
-		{
-			p->color = color + (randk() & numcolors);
-		}
-
-		else
-		{
-			p->color = color;
-		}
-
-		d = (float)(randk() & dirspread);
-
-		for (j = 0; j < 3; j++)
-		{
-			p->org[j] = org[j] + ((randk() & 7) - 4) + d * dir[j];
-			p->vel[j] = crandk() * 20;
-		}
-
-		p->accel[0] = p->accel[1] = 0;
-		p->accel[2] = -PARTICLE_GRAVITY;
-		p->alpha = 1.0;
-
-		p->alphavel = -1.0f / (0.5f + frandk() * alphavel);
-	}
-}
-
